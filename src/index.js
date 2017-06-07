7fa6af5aca69f9262621ba01a526827b96a86207
alicespyglass <alicespyglass@gmail.com>
2017-06-07 23:26:44 +0100

    Alternating turns between X and O

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 
 function Square(props) {
   return (
-    <button className = "square" onClick={props.onClick}>
+    <button className="square" onClick={props.onClick}>
       {props.value}
     </button>
   );
@@ -15,13 +15,17 @@ class Board extends React.Component {
     super();
     this.state = {
       squares: Array(9).fill(null),
+      xIsNext: true,
     };
   }
 
   handleClick(i) {
     const squares = this.state.squares.slice();
-    squares[i] = '🔥';
-    this.setState({squares: squares});
+    squares[i] = this.state.xIsNext ? '☠️' : '🔥';
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext,
+    });
   }
 
   renderSquare(i) {
@@ -34,7 +38,7 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: ☠️';
+    const status = 'Next player: ' + (this.state.xIsNext ? '☠️' : '🔥');
 
     return (
       <div>
